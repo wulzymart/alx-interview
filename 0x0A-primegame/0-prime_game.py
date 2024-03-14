40b575d6d0c9db1c0421c50d0557761e8d59f8a6
Olawole Olawale Martins <olawalemart@gmail.com>
2024-03-14 22:59:57 +0100

    pep formated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,8 +2,9 @@
 """is winner.
 """
 
+
 def SieveOfEratosthenes(n):
- 
+
     # Create a boolean array
     # "prime[0..n]" and initialize
     #  all entries it as true.
@@ -14,21 +15,22 @@ def SieveOfEratosthenes(n):
     prime = [True for i in range(n+1)]
     p = 2
     while (p * p <= n):
- 
+
         # If prime[p] is not
         # changed, then it is a prime
         if (prime[p] == True):
- 
+
             # Update all multiples of p
             for i in range(p * p, n+1, p):
                 prime[i] = False
         p += 1
- 
+
     # Print all prime numbers
     for p in range(2, n+1):
         if prime[p]:
             primes.append(p)
     return primes
+
 
 def isWinner(x, nums):
     """checks winner based on x moves
